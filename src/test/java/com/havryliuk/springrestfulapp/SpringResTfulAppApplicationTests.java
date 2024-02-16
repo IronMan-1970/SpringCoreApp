56ca2b3ca49e4fc19b31dce54e142b711b828871
antihetman <umaks6859@gmail.com>
2024-02-17 00:00:15 +0200

    pushing RESTful App

---
+++ src/test/java/com/havryliuk/springrestfulapp/SpringResTfulAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.havryliuk.springrestfulapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResTfulAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
