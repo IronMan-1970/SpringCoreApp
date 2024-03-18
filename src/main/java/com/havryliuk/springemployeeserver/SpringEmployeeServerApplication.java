14d68a4eeb3441b3f54333f478b13636d6e4dd77
antihetman <umaks6859@gmail.com>
2024-03-18 13:20:00 +0200

    Commiting rest service for employees

---
+++ src/main/java/com/havryliuk/springemployeeserver/SpringEmployeeServerApplication.java
@@ -0,0 +1,13 @@
+package com.havryliuk.springemployeeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmployeeServerApplication.class, args);
+    }
+
+}
